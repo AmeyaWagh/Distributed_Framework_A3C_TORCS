0260210279b2e928edc6fd5c8d334673f07701e0
AmeyaWagh <ameya555@gmail.com>
2017-11-18 16:04:56 -0500

    webserver samples created

---
+++ torcs_central/client.py
@@ -0,0 +1,10 @@
+from websocket import create_connection
+# ws = create_connection("ws://echo.websocket.org/")
+ws = create_connection("ws://130.215.219.68:9090/ws")
+print("Sending 'Hello, World'...")
+ws.send("Hello, World")
+print("Sent")
+print("Receiving...")
+result =  ws.recv()
+print("Received '%s'" % result)
+ws.close()
