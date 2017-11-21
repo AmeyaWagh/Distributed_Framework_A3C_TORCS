907836f4af006be665639703dfdf4c800ca917e8
AmeyaWagh <ameya555@gmail.com>
2017-11-21 15:35:35 -0500

    website changed

--- torcs_central/torcsWebServer.py
+++ torcs_central/torcsWebServer.py
@@ -5,6 +5,8 @@ import tornado.web
 import datetime
 import json
 import os
+import numpy as np
+
 config=json.load(open("./torcs_central/config.json"))
 resourcePath=config['resourcePath']
 
@@ -15,7 +17,15 @@ if not os.path.isdir(resourcePath):
 global resource
 resource=[]
 log=[]
+users = []
+maxReward=250
+def updateUsers():
+    for log_ in log:
+        user = log_[0]
+        if user not in users:
+            users.append(user)
 
+print(os.getcwd())
 def handlerequest(request):
     cmd=request['cmd']
     if cmd=='ping':
@@ -79,14 +89,20 @@ class MyHandler(tornado.web.RequestHandler):
         #     items = [[elem['clientID'],elem['cmd']] for elem in resource]
         # else:
         #     items = []
+        updateUsers()
         items=log
-        self.render("index.html", title="TORCS_A3C", items=items)    
+        self.render("template.html", title="TORCS_A3C", items=items, users=users,maxReward=maxReward)    
 
 if __name__ == '__main__':
+    settings = {
+        "debug": True,
+        "template_path": os.path.join(os.getcwd(),"torcs_central","templates"),
+        "static_path": os.path.join(os.getcwd(),"torcs_central","templates","assets","css")
+        }
     app = tornado.web.Application([ 
         tornado.web.url(r'/', MyHandler),
         tornado.web.url(r'/upload', Upload) ,
-        tornado.web.url(r'/download/(.*)', Download) ])
+        tornado.web.url(r'/download/(.*)', Download) ],**settings)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(9090)
     print('Starting server on port 9090')
